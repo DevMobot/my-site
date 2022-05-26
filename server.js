ec1ca429baa609ca362aa07a31ad48d3df949636
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-05-26 21:42:53 +0530

    site

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express')
+const express = require('express') 
 const app = express()
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 80; // GET PORT TO LISTEN ON
 const path = require('path');
 
-const HomeRouter = require("./routes/home.js");
+const HomeRouter = require("./routes/home.js"); // INCLUDE HOME ROUTER
 
-app.use("/resources", express.static("./css/"))
+app.use(express.static(__dirname + '/public/')); // NOW PUBLIC FILES ARE ACCESSIBLE
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", HomeRouter);
+app.use("/", HomeRouter); // HOME PAGE ROUTES
 
+// LISTENING FOR REQUESTS
 app.listen(port, () => {
-    console.log(`Site open on port ${port}`);
+    console.log(`READY! LISTENING ON PORT ${port}`);
 })
