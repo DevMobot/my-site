89c92c4679b4bc6e2f99f50472e930d85504358f
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-06-01 20:22:32 +0530

    site

--- uninstall-update.sh
+++ uninstall-update.sh
@@ -6,13 +6,13 @@ rm ./my-site/server.js
 
 cp -r ./storage/shared/Old_Site/Site/pages ./my-site/
 cp -r ./storage/shared/Old_Site/Site/routes ./my-site/
-cp -r ./storage/shared/Old_Site/Site/public ./my-site/
+cp -r ./storage/shared/Old_Site/Site/resources ./my-site/
 cp ./storage/shared/Old_Site/Site/server.js ./my-site/
 
 cd ./storage/shared/
 cp -r ./Old_Site/Site/pages ./Site/
 cp -r ./Old_Site/Site/routes ./Site/
-cp -r ./Old_Site/Site/public ./Site/
+cp -r ./Old_Site/Site/resources ./Site/
 cp ./Old_Site/Site/server.js ./Site/
 
 rm -rf ./Old_Site
