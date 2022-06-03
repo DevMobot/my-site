cd13b91d10e6ec30bbee8300358bd8eaa283ed31
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-06-03 15:13:14 +0530

    site

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get('/',function(req, res) {
-    res.send("k")
-    console.log(req.query.search);
+    res.sendFile(path.join(__dirname, "..", "resources/test.json"));     
 });
 
 module.exports = router;
