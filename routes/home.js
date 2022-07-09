2e923aa1be08505d0bc65616947a91981424512f
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-07-09 22:12:02 +0530

    k

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const path = require("path");
-const fs = require("fs");
 const clear_cache = require("../clear_cache.js");
-
-const host = require("../server.js").host;
 
 router.get('/',function(req,res) {
     res.sendFile(path.join(__dirname, "..", "pages/home.html"));
@@ -17,8 +14,18 @@ router.get('/',function(req,res) {
         }];
         req.session.track_index = 0;
     }
-});
-router.get('/player',(req, res) => {
+})
+.get('/player',(req, res) => {
+    if (!req.session.tracks) {
+        req.session.tracks = [{
+            name: "L's Ideology",
+            artist: "DeathNote",
+            path: "../audios/ls-ideology.mp3",
+            image: "../images/art.png",
+            videoId: "P2OA4mQ3mkc"
+        }];
+        req.session.track_index = 0;
+    }
     if (req.query.index) {
         if (req.query.index > req.session.tracks.length || req.query.index < 1) return res.redirect("/player"); 
         req.session.track_index = parseInt(req.query.index)-1;
@@ -27,19 +34,24 @@ router.get('/player',(req, res) => {
     }
     res.sendFile(path.join(__dirname, "..", "pages/player.html"));
 })
-router.get("/exit_player", (req, res) => {
+.get("/exit_player", (req, res) => {
     req.session.track_index = 0;
     req.session.tracks = [];
     res.sendFile(path.join(__dirname, "..", "pages/exit_player.html"));
 })
-router.get("/list", (req, res) => {
+.get("/list", (req, res) => {
     const listid = req.query.id;
     if (!listid) return res.send("ERROR NO ID");
     const listurl = encodeURI("https://youtube.com/playlist?list=" + listid);
     res.redirect("/api/ytp?playlistURL="+listurl+"&disableSeeking=false");
 })
-router.get("/fcc", (req, res) => {
+.get("/fcc", (req, res) => {
     clear_cache();
     res.redirect("/");
 })
+.get("/loading", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "pages/loading.html"));
+})
+
+
 module.exports = router;
