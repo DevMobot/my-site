627089030af98dfb9d268324b1753af0e6bc31fb
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-07-18 21:33:18 +0530

    k

--- resources/js/player.js
+++ resources/js/player.js
@@ -38,40 +38,36 @@ function getCookie(cname) {
   return "";
 }
 
-function Get(yourUrl){
+function Get(yourUrl) {
   var Httpreq = new XMLHttpRequest(); // a new request
-  Httpreq.open("GET",yourUrl,false);
+  Httpreq.open("GET", yourUrl, false);
   Httpreq.send(null);
-  return Httpreq.responseText;          
+  return Httpreq.responseText;
 }
 const mobileCheck = function() {
   let check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  if (window.innerWidth > 500) check = false;
+  else if (window.innerWidth < 500) check = true;
   return check;
 };
+
 if (mobileCheck()) {
   document.querySelector(".queue").style.width = "100%";
   now_playing.style.fontSize = "1rem";
   track_name.style.fontSize = "1.5rem";
   track_artist.style.fontSize = "1rem";
 }
-const media = (title, artist, album, img) => {
+const media = () => {
   if ('mediaSession' in window.navigator) {
       window.navigator.mediaSession.metadata = new MediaMetadata({
-        title: title,
-        artist: artist,
-        album: album,
+        title: track_list[track_index].name.substring(0, 45),
+        artist: track_list[track_index].artist,
+        album: "YouTube",
         artwork: [
-          { src: img,   type: 'image/png' }
+          { src: track_list[track_index].image,   type: 'image/jpg' }
         ]
       });
-      //console.log("kk")
-      navigator.mediaSession.setActionHandler('previoustrack', prevTrack);
-      navigator.mediaSession.setActionHandler('nexttrack', nextTrack);
-      navigator.mediaSession.setActionHandler('play', playpauseTrack);
-      navigator.mediaSession.setActionHandler('pause', playpauseTrack);
-      console.log(navigator.mediaSession)
-      console.log("okkk");
   }
 }
 // Define the tracks that have to be play"ed
@@ -152,13 +148,15 @@ const rateLimit = () => {
 }
 //------------------------------------------------------------------------------------------------
 
-function loadTrack(track_index) {
+async function loadTrack(track_index) {
+
   clearInterval(updateTimer);
   resetValues();
+
   curr_track.src = track_list[track_index].path;
 
   let sessionid = getCookie("connect.sid")
-  let k = Get(`${host_conn}//${host}/api/trackindexsave?ti=${track_index}&session=${sessionid}`);
+  Get(`${host_conn}//${host}/api/trackindexsave?ti=${track_index}&session=${sessionid}`);
   
   curr_track.load();
 
@@ -171,18 +169,11 @@ function loadTrack(track_index) {
   now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
   rateLimit();
-  media(track_list[track_index].name.substring(0, 45), track_list[track_index].artist, "YouTube", track_list[track_index].image);
 
   updateTimer = setInterval(seekUpdate, 1000);
   curr_track.addEventListener("ended", nextTrack);
   
-  /*
-  if ('mediaSession' in navigator) {
-    navigator.mediaSession.setActionHandler('previoustrack', prevTrack);
-    navigator.mediaSession.setActionHandler('nexttrack', nextTrack);
-  } */
-  //random_bg_color();
-
+  //await media(track_index);
 }
 loadTrack(track_index);
 //------------------------------------------------------------------------------------------------
@@ -193,20 +184,21 @@ function playpauseTrack() {
 }
 
 function playTrack() {
-  curr_track.play();
+  curr_track.play().then(() => {
+    media();
+  }).catch(e => {
+    console.log(e);
+  });
+
   isPlaying = true;
-  if ('mediaSession' in navigator) {
-    navigator.mediaSession.playbackState = 'playing';
-  }
+  if ('mediaSession' in window.navigator) window.navigator.mediaSession.playbackState = "playing";
   playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
 
 function pauseTrack() {
   curr_track.pause();
   isPlaying = false;
-  if ('mediaSession' in navigator) {
-    navigator.mediaSession.playbackState = 'paused';
-  }
+  if ('mediaSession' in window.navigator) window.navigator.mediaSession.playbackState = "paused";
   playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
 }
 
@@ -253,14 +245,29 @@ const noSeek = () => {
 noSeek(); 
 //songList();
 
-/*
+
 if ('mediaSession' in navigator) {
-  navigator.mediaSession.setActionHandler('previoustrack', prevTrack);
-  navigator.mediaSession.setActionHandler('nexttrack', nextTrack);
-  navigator.mediaSession.setActionHandler('play', playpauseTrack);
-  navigator.mediaSession.setActionHandler('pause', playpauseTrack);
+  navigator.mediaSession.setActionHandler('previoustrack', () => {
+    prevTrack()
+  });
+  navigator.mediaSession.setActionHandler('nexttrack', () => {
+    nextTrack()
+  });
+  navigator.mediaSession.setActionHandler('play', () => {
+    window.navigator.mediaSession.playbackState = "playing";
+    playpauseTrack()
+  });
+  navigator.mediaSession.setActionHandler('pause', () => {
+    window.navigator.mediaSession.playbackState = "paused";
+    playpauseTrack()
+  });
 }
-*/
+
+//------------------------------------------------------------------------------------------------------
+
+const loadFromQueue = (i) => {
+  track_index = i;
+}
 
 const displayQueue = () => {
   for (let i = 0; i < track_list.length; i++) {
@@ -273,6 +280,9 @@ const displayQueue = () => {
 
     artDiv.classList.add("queueTrackArt");
     listItem.classList.add("queueItem");
+
+    listItem.setAttribute("onclick", `loadTrack(${i});playTrack();loadFromQueue(${i})`);
+    //`redirect('${host_conn}//${host}/player?index=${i+1}')`;
 
     let trackNameText = document.createTextNode(track_list[i].name);
     trackNameSpan.appendChild(trackNameText);
